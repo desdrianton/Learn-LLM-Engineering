ff0f113811cbf066354d601a3111ee65676468fd
desdrianton <desdrianton@gmail.com>
2025-01-28 07:47:34 +0700

    Update yt_qna.py
    Include all LLM (chatgpt, llama, claude, gemini), so we can compare them

--- youtube_things/yt_qna.py
+++ youtube_things/yt_qna.py
@@ -1,6 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
-from core.llm_connector import ChatGPTConnector
+from core.llm_connector import ChatGPTConnector, OllamaConnector, ClaudeConnector, GeminiConnector
+
 
 def get_transcript(video_id, language='id'):
     try:
@@ -10,41 +11,44 @@ def get_transcript(video_id, language='id'):
         print(f"Error fetching transcript: {e}")
         return None
 
+
+def seperator(info: str):
+    print(f"{'=' * 100}\n{info}\n{'=' * 100}\n\n\n")
+
+
+def ask_question(i: int, system_prompt: str, question: str):
+    print(f"{'.' * 80} QnA {i}")
+    print(f"Ask:\n{question}\n\n")
+    answer = llm_connector.ask(system_prompt=system_prompt, user_prompt=question)
+    print(f"Answer:\n{answer}\n\n")
+
 # ======================================================================================================================
 # Run
 # ======================================================================================================================
+seperator("Video: Tutorial WDIO WebdriverIO #4 - Method Pada Element Di WebdriverIO")
 llm_connector = ChatGPTConnector()
+# llm_connector = OllamaConnector()
+# llm_connector = ClaudeConnector()
+# llm_connector = GeminiConnector() # Error !!! Need bugfixing
 yt_video_id = "tvAQGusmiDI"
 
 transcript_text = get_transcript(yt_video_id)
-system_prompt = f"""
+_system_prompt = f"""
 Engkau adalah trainer yang sangat sabar dan fun dalam menjelaskan materi. Muridmu kebanyakan adalah gen-z, sehingga jangan terlalu kaku dalam menjawab. Tugasmu adalah:
 - Menjawab pertanyaan dari murid
 - Menjawab dengan sabar
 - Pergunakan bullet point bila memungkinkan
 
-Materinya adalah sebagai berikut: {transcript_text} 
+Materinya adalah sebagai berikut: {transcript_text}
+Engkau tidak boleh menjawab pertanyaan diluar materi!!
+Engkau tidak boleh menjawab pertanyaan diluar materi meskipus satu kalimat saja
 """
 
-print(f"{'=' * 100}")
-print("Video: Tutorial WDIO WebdriverIO #4 - Method Pada Element Di WebdriverIO")
-print(f"{'=' * 100}")
-print("\n\n")
-
-ask1 = "Jelaskan materi ini seakan-akan saya anak kelas 5 SD"
-answer1 = llm_connector.ask(system_prompt=system_prompt, user_prompt=ask1)
-print(f"{'.'*80} QnA 1")
-print(f"Ask:\n{ask1}\n\n")
-print(f"Answer:\n{answer1}\n\n")
-
-ask2 = "Berikan contoh code yang lain"
-answer2 = llm_connector.ask(system_prompt="", user_prompt=ask2)
-print(f"{','*80} QnA 2")
-print(f"Ask:\n{ask2}\n\n")
-print(f"Answer:\n{answer2}\n\n")
-
-ask3 = "Apakah di video ini dijelaskan cara menguji captcha?"
-answer3 = llm_connector.ask(system_prompt="", user_prompt=ask3)
-print(f"{'.'*80} QnA 3")
-print(f"Ask:\n{ask3}\n\n")
-print(f"Answer:\n{answer3}\n\n")
+seperator("System Prompt")
+print(_system_prompt)
+ask_question(1, system_prompt=_system_prompt, question="Jelaskan materi ini seakan-akan saya anak kelas 5 SD")
+ask_question(2, system_prompt="", question="Berikan contoh code yang lain")
+ask_question(3, system_prompt="", question="Apakah contoh code nya untuk web https://www.tokopedia.com? Mohon ubah semua code di atas")
+ask_question(4, system_prompt="", question="Apakah di video ini dijelaskan cara menguji captcha?")
+ask_question(5, system_prompt="", question="Siapakah Jokowi itu?")
+ask_question(6, system_prompt="", question="Jelaskan cara mensetup / meng-konfigure webdriver.io")
